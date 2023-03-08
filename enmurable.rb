4a4eff7aeb3b3e368dddb52149e59aebbd8390bf
Wali Muhammad <luckywali0986@gmail.com>
2023-03-09 00:42:49 +0500

    fix linter errors

--- enmurable.rb
+++ enmurable.rb
@@ -3,10 +3,12 @@ module MyEnumerable
     each { |item| return false unless block.call(item) }
     true
   end
+
   def any?(&block)
     each { |item| return true if block.call(item) }
     false
   end
+
   def filter(&block)
     result = []
     each { |item| result << item if block.call(item) }
